5dfc40bad4cdac00fde46d84f21399fd04d2636a
luoshizhu <luosz09@chinatelecom.cn>
2023-09-09 23:53:57 +0800

    add bulma css

---
+++ src/test/java/com/farhantom/hellowiki/HelloWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhantom.hellowiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
